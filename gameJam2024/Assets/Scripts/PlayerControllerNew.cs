40d24639c351164567d4e93a3bf3ef88aa3a8c33
Jae Ng (Jing Han) <34706748+jackhelvetica@users.noreply.github.com>
2024-02-28 13:45:51 +0800

    edited PlayerInputHandler and PlayerControllerNew

--- gameJam2024/Assets/Scripts/PlayerControllerNew.cs
+++ gameJam2024/Assets/Scripts/PlayerControllerNew.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(CharacterController))]
 public class PlayerControllerNew : MonoBehaviour
@@ -11,6 +12,8 @@ public class PlayerControllerNew : MonoBehaviour
     private float playerSpeed = 2.0f;
     private float jumpHeight = 1.0f;
     private float gravityValue = -9.81f;
+
+    private Vector3 move;
 
     private void Awake()
     {
@@ -25,7 +28,7 @@ public class PlayerControllerNew : MonoBehaviour
             playerVelocity.y = 0f;
         }
 
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        
         controller.Move(move * Time.deltaTime * playerSpeed);
 
         if (move != Vector3.zero)
@@ -41,5 +44,11 @@ public class PlayerControllerNew : MonoBehaviour
 
         playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Vector2 movement = context.ReadValue<Vector2>();
+        move = new Vector3(movement.x, 0, movement.y);
     }
 }
--- gameJam2024/Assets/Scripts/PlayerInputHandler.cs
+++ gameJam2024/Assets/Scripts/PlayerInputHandler.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerInputHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject mountPrefab;
+    PlayerControllerNew playerControllerNew;
+
+    private void Awake()
     {
-        
+        if (mountPrefab != null)
+        {
+            playerControllerNew = mountPrefab.GetComponent<PlayerControllerNew>();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMove(InputAction.CallbackContext context)
     {
-        
+        playerControllerNew.OnMove(context);
     }
 }
