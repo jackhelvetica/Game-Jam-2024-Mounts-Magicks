90c55841d28df04a23e5f951f6c864acc58e1d13
clarishatan <tanclarisha@gmail.com>
2024-03-21 12:39:20 +0800

    Added DOTween and more character handler stuff

--- gameJam2024/Assets/Scripts/CharacterHandler.cs
+++ gameJam2024/Assets/Scripts/CharacterHandler.cs
@@ -1,8 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class CharacterHandler : MonoBehaviour
 {
@@ -19,6 +22,9 @@ public class CharacterHandler : MonoBehaviour
     public List<PlayerInput> playerInputList = new List<PlayerInput>();
     public GameObject characterSelectScreen;
     public Button readyButton;
+
+    //Countdown
+    public List<GameObject> countdownObjects = new List<GameObject>();
     
     void Start()
     {
@@ -38,6 +44,7 @@ public class CharacterHandler : MonoBehaviour
             player.transform.position = spawnPointA;
             player.transform.rotation = Quaternion.Euler(0, 90, 0);
             player.tag = "Mount1";
+            player.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
 
             playersImageList[index].SetActive(true);
             playerInputList.Add(player);
@@ -50,6 +57,7 @@ public class CharacterHandler : MonoBehaviour
             player.transform.position = spawnPointA;
             player.transform.rotation = Quaternion.Euler(0, 90, 0);
             player.tag = "Rider1";
+            player.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
 
             playersImageList[index].SetActive(true);
             playerInputList.Add(player);
@@ -62,6 +70,7 @@ public class CharacterHandler : MonoBehaviour
             player.transform.position = spawnPointB;
             player.transform.rotation = Quaternion.Euler(0, 270, 0);
             player.tag = "Rider2";
+            player.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
 
             playersImageList[index].SetActive(true);
             playerInputList.Add(player);
@@ -74,14 +83,52 @@ public class CharacterHandler : MonoBehaviour
             player.transform.position = spawnPointB;
             player.transform.rotation = Quaternion.Euler(0, 270, 0);
             player.tag = "Mount2";
+            player.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
 
             playersImageList[index].SetActive(true);
             playerInputList.Add(player);
+
+            index++;
         }
-        else if (index > 3)
+        else if (index == 4)
         {
-            index = 3;
+            readyButton.interactable = true;
+            readyButton.Select();
         }
+    }
+
+    public void Ready()
+    {
+        characterSelectScreen.SetActive(false);
+        StartCoroutine(Countdown(3));
+    }
+
+    public IEnumerator Countdown(int seconds)
+    {
+        //Tween variables
+        Vector3 punchScale = new Vector3(0.5f, 0.5f, 0.5f);
+        float punchDuration = 1;
+        int punchVibrato = 0;
+        float punchElasticity = 0;
+
+        int count = seconds; //count = 3;
+        while (count > -1) //will stop once count = 0
+        {
+            countdownObjects[count].SetActive(true);
+            countdownObjects[count].transform.DOPunchScale(punchScale, punchDuration, punchVibrato, punchElasticity);
+            yield return new WaitForSeconds(1);
+            countdownObjects[count].SetActive(false);
+            count--;
+        }
+
+        //countdownObjects[count].spriteRenderer.DOFade(0, 1);
+
+        //Start game
+        //foreach (var playerInput in playerInputList)
+        //{
+        //    //GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+        //    //Freeze rotation x and z
+        //}     
     }
 
     public void ResetPlayers()
